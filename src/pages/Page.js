9dd16be548c868e8aee86be2f8ba478af3485afc
Yaiza <yaizavallejo16@gmail.com>
2021-08-18 16:11:07 +0200

    Change background color

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Page.scss';
 import Header from '../components/Header';
 
 const Page = () => (
   <div>
     <Header />
+    <div className="main-container" />
   </div>
 );
 
