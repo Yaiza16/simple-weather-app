c978934ce6e0fb9f1d4f26ead666c232d9854d89
Yaiza <yaizavallejo16@gmail.com>
2021-08-19 17:39:36 +0200

    Import custom hook

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './Page.scss';
 import Header from '../components/Header';
 import Form from '../components/Form';
+import useForecast from '../hooks/useForecast';
+import Error from '../components/Error/Error';
 
-const Page = () => (
-  <div>
-    <Header />
-    <div className="main-container">
-      <Form />
+const Page = () => {
+  const { submitRequest, isError, errorData } = useForecast();
+
+  return (
+    <div>
+      <Header />
+      <div className="main-container">
+        <Form submitRequest={submitRequest} />
+        {isError && <Error message={errorData} />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Page;
