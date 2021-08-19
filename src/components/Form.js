57a4807e10ed0a9d37fa257e6bedb402e29e7d3b
Yaiza <yaizavallejo16@gmail.com>
2021-08-19 17:40:15 +0200

    Fix errors in submit function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.scss';
 import { IconLocation } from './helpers/Icons';
 
-const Form = () => (
-  <form className="form-location">
-    <label htmlFor="location">Location</label>
-    <div className="input-container">
-      <input
-        className="input-location"
-        type="text"
-        placeholder="e.g. London"
-        id="location"
-        required
-      />
+const Form = ({ submitRequest }) => {
+  const [location, setLocation] = useState('');
 
-      <IconLocation />
-    </div>
-    <button type="submit" className="form-button">
-      Search
-    </button>
-  </form>
-);
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    submitRequest(location);
+  };
+
+  return (
+    <form className="form-location" onSubmit={submitForm}>
+      <label htmlFor="location">Location</label>
+      <div className="input-container">
+        <input
+          className="input-location"
+          type="text"
+          placeholder="e.g. London"
+          id="location"
+          required
+          value={location}
+          autoComplete="off"
+          onChange={(e) => setLocation(e.target.value)}
+        />
+
+        <IconLocation />
+      </div>
+      <button type="submit" className="form-button" onClick={submitForm}>
+        Search
+      </button>
+    </form>
+  );
+};
 
 export default Form;
