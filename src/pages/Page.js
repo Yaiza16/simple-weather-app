241068f0cfc3dcf0819aa05798356f08f54d738d
Yaiza <yaizavallejo16@gmail.com>
2021-08-19 18:06:34 +0200

    Add isLoading

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -4,16 +4,18 @@ import Header from '../components/Header';
 import Form from '../components/Form';
 import useForecast from '../hooks/useForecast';
 import Error from '../components/Error/Error';
+import Loading from '../components/vendors/loading/Loading';
 
 const Page = () => {
-  const { submitRequest, isError, errorData } = useForecast();
+  const { submitRequest, isLoading, isError, errorData } = useForecast();
 
   return (
     <div>
       <Header />
       <div className="main-container">
-        <Form submitRequest={submitRequest} />
+        {!isLoading && <Form submitRequest={submitRequest} />}
         {isError && <Error message={errorData} />}
+        {isLoading && <Loading />}
       </div>
     </div>
   );
