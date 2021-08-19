f6310de8b8b090f40917916f3494913fa7ba90cd
Yaiza <yaizavallejo16@gmail.com>
2021-08-19 18:04:58 +0200

    Add loading component

---
+++ src/components/vendors/loading/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loading.scss';
+
+const Loading = () => (
+  <div className="loader-container">
+    <span className="loader" />
+  </div>
+);
+
+export default Loading;
