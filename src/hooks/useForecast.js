4209c0502dc78927599408d6e4516887096e6318
Yaiza <yaizavallejo16@gmail.com>
2021-08-20 11:02:30 +0200

    Fix problem with value return from api calls

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import getCurrentDayData from '../helpers/getCurrentDayData';
 
 const BASE_URL_LATLON = 'https://api.openweathermap.org/data/2.5/forecast';
 const BASE_URL_FORECAST = 'https://api.openweathermap.org/data/2.5/onecall';
@@ -10,12 +11,12 @@ const useForecast = () => {
   const [errorData, setErrorData] = useState('');
 
   const getLatLon = async (location) => {
+    let coord = null;
     await axios(BASE_URL_LATLON, {
       params: { q: location, appid: process.env.REACT_APP_API_KEY }
     })
       .then(({ data }) => {
-        const { coord } = data.city;
-        return coord;
+        coord = data.city.coord;
       })
       .catch((error) => {
         if (error.response.status === 404) {
@@ -24,9 +25,11 @@ const useForecast = () => {
           setErrorData('There is not such location');
         }
       });
+    return coord;
   };
 
   const getForecast = async (latLon) => {
+    let response = null;
     const exludeParams = 'current,minutely,hourly';
     await axios(BASE_URL_FORECAST, {
       params: {
@@ -36,12 +39,15 @@ const useForecast = () => {
         appid: process.env.REACT_APP_API_KEY
       }
     })
-      .then(({ data }) => data)
+      .then(({ data }) => {
+        response = data;
+      })
       .catch(() => {
         setIsError(true);
         setIsLoading(false);
         setErrorData('Some error happened');
       });
+    return response;
   };
 
   const submitRequest = async (location) => {
@@ -49,11 +55,12 @@ const useForecast = () => {
     setIsError(false);
 
     const coord = await getLatLon(location);
-    console.log(isError);
+    // console.log(isError);
     if (!coord) return;
     const data = await getForecast(coord);
 
     console.log(data);
+    console.log(getCurrentDayData(data));
   };
 
   return { submitRequest, isLoading, isError, errorData };
