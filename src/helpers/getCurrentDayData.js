e47bffa25d8e1708271e5d4e6a2ea6ec253747fd
Yaiza <yaizavallejo16@gmail.com>
2021-08-21 12:19:35 +0200

    Add new data

--- src/helpers/getCurrentDayData.js
+++ src/helpers/getCurrentDayData.js
@@ -18,7 +18,9 @@ const getCurrentDayData = (data) => ({
   temp: Math.round(Number(data.daily[0].temp.day) - 271.15), // Kelvin to celsius
   tempMax: Math.round(Number(data.daily[0].temp.max) - 271.15),
   tempMin: Math.round(Number(data.daily[0].temp.min) - 271.15),
-  pop: data.daily[0].pop, // Probability of precipitation
+  pop: data.daily[0].pop * 100, // Probability of precipitation
+  humidity: data.daily[0].humidity,
+  pressure: data.daily[0].pressure,
   windSpeed: data.daily[0].wind_speed,
   weather: data.daily[0].weather[0].description
 });
